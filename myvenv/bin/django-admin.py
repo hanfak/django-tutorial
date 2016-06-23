ff56afc78413574097656ee20a192736471b9d5f
Hanif Fakira <fakira.work@gmail.com>
2016-06-23 13:05:54 +0100

    initial commit, setup django

---
+++ myvenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/hanfak/Dropbox/CODING/PERSONAL-PROJECTS/python-learning/django-tutorial-1/myvenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
